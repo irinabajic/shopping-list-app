fee65ad9c472b065e4980387da1de400c7376cb2
Irina <irinabajic21@gmail.com>
2025-08-13 19:19:37 +0200

    Podesen Firebase URL i HttpClient za REST

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseDbUrl: 'https://shopping-list-app-db526-default-rtdb.europe-west1.firebasedatabase.app/'
 };
 
 /*
